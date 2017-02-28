6b1e5f421b5a3fdd99709ab45369b3d12c7413a5
User-PC\User <cilliancleland@gmail.com>
2017-02-28 16:56:33 +1100

    http - fetch

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -15,6 +15,15 @@ app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath
 }));
 
+app.get('/users', function( req, res) {
+  // hard-coded for simplicity
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+    {"id": 2, "firstName": "ted", "lastName": "sands", "email": "ted@gmail.com"},
+    {"id": 3, "firstName": "fred", "lastName": "gruff", "email": "fred@gmail.com"}
+  ]);
+});
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'))
 });
